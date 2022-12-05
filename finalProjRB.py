b7a41d219adf25d3b9c9b06232a6bb0ab49adc8f
rishma <rishma@umich.edu>
2022-12-05 11:06:08 -0500

    made Villagers and Personalities tables

--- finalProjRB.py
+++ finalProjRB.py
@@ -14,18 +14,48 @@ def get_villagers():
     villagers_dict = json.loads(r)
     return villagers_dict
 
+def create_personalities_table(cur, conn, villagers_dict):
+    personalities_list = []
+    for villager in villagers_dict:
+        personality = villagers_dict[villager]['personality']
+        if personality not in personalities_list:
+            personalities_list.append(personality)
+    cur.execute("CREATE TABLE IF NOT EXISTS Personalities (id INTEGER PRIMARY KEY, personality TEXT UNIQUE)")
+    for i in range(len(personalities_list)):
+        cur.execute("INSERT OR IGNORE INTO Personalities (id,personality) VALUES (?,?)",(i,personalities_list[i]))
+    conn.commit()
+
 def create_villagers_table(cur, conn, villagers_dict):
-    cur.execute("CREATE TABLE IF NOT EXISTS Villagers (villager_id INTEGER PRIMARY KEY, name TEXT, personality TEXT, birthday TEXT, species TEXT, gender TEXT, catchphrase TEXT)")
+    cur.execute("CREATE TABLE IF NOT EXISTS Villagers (villager_id INTEGER PRIMARY KEY, name TEXT, personality_id INTEGER, birthday TEXT, species TEXT, gender TEXT, catchphrase TEXT)")
     conn.commit()
     for villager in villagers_dict:
         # print(villagers_dict[villager])
         villager_id = villagers_dict[villager]['id']
-        print(villager_id)
+        # print(villager_id)
+        name = villagers_dict[villager]['name']['name-USen']
+        # print(name)
+        personality = villagers_dict[villager]['personality']
+        cur.execute('SELECT id FROM Personalities WHERE Personalities.personality = "' + personality + '"')
+        for row in cur:
+            personality_id = row[0]
+        # print(personality)
+        birthday = villagers_dict[villager]['birthday']
+        # note: birthday is in the format d/m
+        # print(birthday)
+        species = villagers_dict[villager]['species']
+        # print(species)
+        gender = villagers_dict[villager]['gender']
+        # print(gender)
+        catchphrase = villagers_dict[villager]['catch-phrase']
+        # print(catchphrase)
+        cur.execute('INSERT OR IGNORE INTO Villagers (villager_id, name, personality_id, birthday, species, gender, catchphrase) VALUES (?, ?, ?, ?, ?, ?, ?)', (villager_id, name, personality_id, birthday, species, gender, catchphrase))
+        conn.commit()
 
 def main():
     cur, conn = open_database('acnh.db')
     villagers_dict = get_villagers()
     # print(villagers_dict)
+    create_personalities_table(cur, conn, villagers_dict)
     create_villagers_table(cur, conn, villagers_dict)
 
 if __name__ == "__main__":
