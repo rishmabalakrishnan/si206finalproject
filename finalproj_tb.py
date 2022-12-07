40fec658734fd59a886c7a3e08db00ba7f629cb2
Tyra Briscoe <tyrabriscoe@Tyras-MacBook-Air.local>
2022-12-07 15:17:55 -0500

    add recipes to table

--- finalproj_tb.py
+++ finalproj_tb.py
@@ -23,37 +23,53 @@ def make_database(db_name):
     cur = conn.cursor()
     return cur, conn
 
-def create_recipes_table(cur, conn):
-    cur.execute("CREATE TABLE IF NOT EXISTS recipes (item_id INTEGER PRIMARY KEY, name TEXT, wood INTEGER, hardwood INTEGER, softwood INTEGER, stone INTEGER, iron_nugget INTEGER, clay INTEGER, tree_branch INTEGER)")
 
+def create_availability_table(recipes_dict, cur, conn):
+    avail_list = []
+    for recipe in recipes_dict:
+        availability = recipe["availability"][0]["from"]
+        if availability not in avail_list:
+            avail_list.append(availability)
+    cur.execute("CREATE TABLE IF NOT EXISTS availability (id INTEGER PRIMARY KEY, availability TEXT UNIQUE)")
+    for i in range(len(avail_list)):
+        cur.execute("INSERT OR IGNORE INTO availability (id, availability) VALUES (?,?)",(i,avail_list[i]))
     conn.commit()
 
 
+def create_recipes_table(cur, conn):
+    cur.execute("CREATE TABLE IF NOT EXISTS recipes (item_id INTEGER PRIMARY KEY, name TEXT, availability_id INTEGER, wood INTEGER, hardwood INTEGER, softwood INTEGER, stone INTEGER, iron_nugget INTEGER, clay INTEGER, tree_branch INTEGER)")
+
+    conn.commit()
+
 def add_recipes_to_table (recipes_dict, cur, conn):
-    
+
     for recipe in recipes_dict:
         item_id = recipe["serial_id"]
         name = recipe["name"]
 
+        cur.execute("SELECT id FROM availability WHERE availability = ?", (recipe["availability"][0]["from"],))
+        availability_id = int(cur.fetchone()[0])
+
     # exclude wallpaper, flooring, wreaths, wands
         if "wall" in name or "flooring" in name or "wreath" in name or "wand" in name:
             continue
+        
+        material_list= ["wood", "hardwood", "softwood", "tree branch", "clay", "iron nugget", "stone"]
 
         for material in recipe["materials"]:
+        #     material_name = material["name"]
+        #     material_count = material['count']
+        #     if material_name in material_list:
+        #         recipe_materials[material_name] = material_count #recipe_materials.get(material_name, 0) + 1 
+        # print(recipe_materials)
 
-            material_list= ["wood", "hardwood", "softwood", "tree_branch", "clay", "iron_nugget", "stone"]
             if material["name"] not in material_list:
                 continue
             
-            # recipe_materials = {}
             if material["name"] == "wood":
-                # recipe_materials[material] = material["count"]
-                # print(recipe_materials[material])
                 wood = material["count"]
             else:
                 wood = 0
-
-            # print(recipe_materials[wood])
         
             if material["name"] == "hardwood":
                 hardwood = material["count"]
@@ -85,26 +101,14 @@ def add_recipes_to_table (recipes_dict, cur, conn):
             else:
                 tree_branch = 0
 
-            cur.execute("INSERT OR IGNORE INTO recipes (item_id, name, wood, hardwood, softwood, stone, iron_nugget, clay, tree_branch) VALUES (?,?,?,?,?,?,?,?,?)",(item_id, name, wood, hardwood, softwood, stone, iron_nugget, clay, tree_branch))
+            cur.execute("INSERT OR IGNORE INTO recipes (item_id, name, availability_id, wood, hardwood, softwood, stone, iron_nugget, clay, tree_branch) VALUES (?,?,?,?,?,?,?,?,?,?)" , (item_id, name, availability_id, wood, hardwood, softwood, stone, iron_nugget, clay, tree_branch))
         conn.commit()
-
-
-def create_availability_table(recipes_dict, cur, conn):
-    avail_list = []
-    for recipe in recipes_dict:
-        availability = recipe["availability"][0]["from"]
-        if availability not in avail_list:
-            avail_list.append(availability)
-    cur.execute("CREATE TABLE IF NOT EXISTS availability (id INTEGER PRIMARY KEY, availability TEXT UNIQUE)")
-    for i in range(len(avail_list)):
-        cur.execute("INSERT OR IGNORE INTO availability (id, availability) VALUES (?,?)",(i,avail_list[i]))
-    conn.commit()
 
 def main():
     cur, conn = make_database('acnh.db')
-    create_recipes_table(cur, conn)
     recipes_dict = get_recipes(api_key)
-    add_recipes_to_table(recipes_dict, cur, conn)
     create_availability_table(recipes_dict, cur, conn)
+    create_recipes_table(cur, conn)
+    add_recipes_to_table(recipes_dict, cur, conn)
 
 main()
